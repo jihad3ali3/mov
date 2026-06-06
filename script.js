181559ba01ca54a66ed99ea9924020b47410bbf7
Jihad Ali Ghalib Almesbahi <jihad3ali3@gmail.com>
2026-06-06 06:25:32 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const cameraFeed = document.getElementById('cameraFeed');
-
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(stream => {
-    cameraFeed.srcObject = stream;
-  })
-  .catch(error => {
-    console.error('Error accessing camera:', error);
-  });
